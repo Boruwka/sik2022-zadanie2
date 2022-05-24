117d2b12c49d60c48e582efc1348667d137d294c
Jagoda Bracha <jb429153@students.mimuw.edu.pl>
2022-05-24 14:49:33 +0200

    przerobienie na watki

--- client.cpp
+++ client.cpp
@@ -1,165 +1,103 @@
 #include <iostream>
+#include <thread>
 #include <boost/asio.hpp>
 #include <boost/enable_shared_from_this.hpp>
 #include <boost/bind/bind.hpp>
 #include <boost/array.hpp>
 #include <boost/program_options.hpp>
 
-enum Direction
+size_t max_length = 65536;
 
-enum InputMessage 
+enum class Direction 
+{
+    Up,
+    Right,
+    Down,
+    Left
+};
+
+enum class InputMessage 
 {
     PlaceBomb,
     PlaceBlock,
-    Move { direction: Direction },
-}
+    Move //{ direction: Direction },
+};
 
-void handle_message_from_server(char data_[], std::size_t& length)
+void handle_message_from_server(char data[], std::size_t& length)
 {
+    fprintf(stderr, "tcp dostalismy %d bytow\n", length);
+    std::cerr << data << std::endl;
     // tu czytamy co nam wysłał serwer 
     // zastanawiamy się nad tym 
     // i wklejamy do data odpowiedź jak będziemy chcieli żeby ją wysłać to zmieniamy length i data i kończymy funkcję
     // chyba czekamy na gui też tutaj 
-    // tu będzie coś asynchronicznego
+    // tu będzie aktywne czekanie na gui
 }
 
-void handle_message_from_gui(char data_[], std::size_t& length)
+void handle_message_from_gui(char data[], std::size_t& length)
 {
+    fprintf(stderr, "udp dostalismy %d bytow\n", length);
+    std::cerr << data << std::endl;
     // tu czytamy co nam wysłało gui 
     // zastanawiamy się nad tym 
     // i wklejamy do data odpowiedź jak będziemy chcieli żeby ją wysłać to zmieniamy length i data i kończymy funkcję
-    // asynchroniczne czekanie na serwer
+    // aktywne czekanie na serwer
 }
 
-class udp_server
+void udp_server(boost::asio::io_context& io_context, unsigned short port_listening, unsigned short port_sending)
 {
-public:
-    udp_server(boost::asio::io_context& io_context, short port)
-        : io_context_(io_context),
-        socket_(io_context,             boost::asio::ip::udp::udp::endpoint(boost::asio::ip::udp::udp::v6(), port))
+    fprintf(stderr, "witamy w serwerze udp\n");
+    boost::asio::ip::udp::udp::socket sock_listening(io_context, boost::asio::ip::udp::udp::endpoint(boost::asio::ip::udp::udp::v6(), port_listening));
+    boost::asio::ip::udp::udp::socket sock_sending(io_context, boost::asio::ip::udp::udp::endpoint(boost::asio::ip::udp::udp::v6(), port_sending));
+    for (;;)
     {
-        fprintf(stderr, "witamy w konstruktorze\n");
-        socket_.async_receive_from(
-        boost::asio::buffer(data_, max_length), sender_endpoint_,
-        boost::bind(&udp_server::handle_receive_from, this,
-        boost::asio::placeholders::error,
-        boost::asio::placeholders::bytes_transferred));
+        char data[max_length];
+        boost::asio::ip::udp::udp::endpoint sender_endpoint;
+        size_t length = sock_listening.receive_from(
+            boost::asio::buffer(data, max_length), sender_endpoint);
+        handle_message_from_gui(data, length);
+        sock_sending.send_to(boost::asio::buffer(data, length), sender_endpoint);
     }
+}
 
-    void handle_receive_from(const boost::system::error_code& error,
-        size_t bytes_recvd)
+void session(boost::asio::ip::tcp::tcp::socket sock)
+{
+    fprintf(stderr, "witamy w sesji tcp\n");
+    try
     {
-        fprintf(stderr, "dostalem cos z udp %d bytow\n", bytes_recvd);
-        std::cerr << data_ << std::endl;
-        if (!error && bytes_recvd > 0)
+        sock.set_option(boost::asio::ip::tcp::tcp::no_delay(true));
+        for (;;)
         {
-            socket_.async_send_to(
-            boost::asio::buffer(data_, bytes_recvd), sender_endpoint_,
-            boost::bind(&udp_server::handle_send_to, this,
-            boost::asio::placeholders::error,
-            boost::asio::placeholders::bytes_transferred));
+            char data[max_length];
+
+            boost::system::error_code error;
+            size_t length = sock.read_some(boost::asio::buffer(data, max_length), error);
+            if (error == boost::asio::error::eof)
+                break; // Connection closed cleanly by peer.
+            else if (error)
+                throw boost::system::system_error(error); // Some other error.
+
+            handle_message_from_server(data, length);
+            boost::asio::write(sock, boost::asio::buffer(data, length));
         }
-        else
-        {
-            socket_.async_receive_from(
-            boost::asio::buffer(data_, max_length), sender_endpoint_,
-            boost::bind(&udp_server::handle_receive_from, this,
-            boost::asio::placeholders::error,
-            boost::asio::placeholders::bytes_transferred));
-        }
     }
-
-    void handle_send_to(const boost::system::error_code& error, size_t bytes_sent)
+    catch (std::exception& e)
     {
-        socket_.async_receive_from(
-        boost::asio::buffer(data_, max_length), sender_endpoint_,
-        boost::bind(&udp_server::handle_receive_from, this,
-        boost::asio::placeholders::error,
-        boost::asio::placeholders::bytes_transferred));
+        std::cerr << "Exception in thread: " << e.what() << "\n";
     }
+}
 
-private:
-    boost::asio::io_context& io_context_;
-    boost::asio::ip::udp::udp::socket socket_;
-    boost::asio::ip::udp::udp::endpoint sender_endpoint_;
-    enum { max_length = 1024 };
-    char data_[max_length];
-};
-
-class session
-    : public std::enable_shared_from_this<session>
+void tcp_server(boost::asio::io_context& io_context, unsigned short port)
 {
-public:
-    session(boost::asio::ip::tcp::tcp::socket socket)
-        : socket_(std::move(socket))
+    fprintf(stderr, "witamy w serwerze tcp\n");
+    boost::asio::ip::tcp::tcp::acceptor a(io_context, boost::asio::ip::tcp::tcp::endpoint(boost::asio::ip::tcp::tcp::v6(), port));
+    for (;;)
     {
+        std::thread(session, a.accept()).detach();
     }
+}
 
-    void start()
-    {
-        do_read();
-    }
-
-private:
-    void do_read()
-    {
-        auto self(shared_from_this());
-        socket_.async_read_some(boost::asio::buffer(data_, max_length),
-            [this, self](boost::system::error_code ec, std::size_t length)
-        {
-            fprintf(stderr, "dostalem cos z tcp, %d bajtow\n", length);
-            if (!ec)
-            {
-                handle_message_from_server(data_, length);
-                do_write(length);
-            }
-        });
-    }
-
-    void do_write(std::size_t length)
-    {
-        auto self(shared_from_this());
-        boost::asio::async_write(socket_, boost::asio::buffer(data_, length),
-            [this, self](boost::system::error_code ec, std::size_t /*length*/)
-        {
-            if (!ec)
-            {
-                do_read();
-            }
-        });
-    }
-
-    boost::asio::ip::tcp::tcp::socket socket_;
-    enum { max_length = 1024 };
-    char data_[max_length];
-};
-
-class tcp_server
-{
-public:
-  tcp_server(boost::asio::io_context& io_context, short port)
-    : acceptor_(io_context, boost::asio::ip::tcp::tcp::endpoint(boost::asio::ip::tcp::tcp::v6(), port))
-  {
-    do_accept();
-  }
-
-private:
-  void do_accept()
-  {
-    acceptor_.async_accept(
-        [this](boost::system::error_code ec, boost::asio::ip::tcp::tcp::socket socket)
-        {
-          if (!ec)
-          {
-            std::make_shared<session>(std::move(socket))->start();
-          }
-
-          do_accept();
-        });
-  }
-
-  boost::asio::ip::tcp::acceptor acceptor_;
-};
+void debug_1() {}
 
 int main(int argc, char *argv[])
 {
@@ -175,7 +113,7 @@ int main(int argc, char *argv[])
     description.add_options()
         ("help,h", "Get help")
         ("gui-address,d", boost::program_options::value<std::string>(), "Gui address")
-        ("playner-name,n", boost::program_options::value<std::string>(), "Player name")
+        ("player-name,n", boost::program_options::value<std::string>(), "Player name")
         ("port,p", boost::program_options::value<uint16_t>(), "Port for listening gui")
         ("server-address,s", boost::program_options::value<std::string>(), "Server address");
 
@@ -190,8 +128,8 @@ int main(int argc, char *argv[])
 
     if (parameter_map.count("gui-address"))
     {
-        std::string gui_address = parameter_map["gui-address"];
-        last_colon_pos = gui_address.rfind(':');
+        std::string gui_address = parameter_map["gui-address"].as<std::string>();
+        size_t last_colon_pos = gui_address.rfind(':');
         std::string host_part = gui_address.substr(0, last_colon_pos);
         std::string port_part = gui_address.substr(last_colon_pos + 1);
         port_sending_gui = (uint16_t)std::stoi(port_part);
@@ -205,7 +143,7 @@ int main(int argc, char *argv[])
 
     if (parameter_map.count("player-name"))
     {
-        player_name = parameter_map["player-name"];
+        player_name = parameter_map["player-name"].as<std::string>();
     }
     else
     {
@@ -215,7 +153,7 @@ int main(int argc, char *argv[])
 
     if (parameter_map.count("port"))
     {
-        port = parameter_map["port"];
+        port_server = parameter_map["port"].as<uint16_t>();
     }
     else
     {
@@ -225,10 +163,10 @@ int main(int argc, char *argv[])
 
     if (parameter_map.count("server-address"))
     {
-        std::string server_address = parameter_map["server-address"];
-        last_colon_pos = gui_address.rfind(':');
-        std::string host_part = gui_address.substr(0, last_colon_pos);
-        std::string port_part = gui_address.substr(last_colon_pos + 1);
+        std::string server_address = parameter_map["server-address"].as<std::string>();
+        size_t last_colon_pos = server_address.rfind(':');
+        std::string host_part = server_address.substr(0, last_colon_pos);
+        std::string port_part = server_address.substr(last_colon_pos + 1);
         port_server = (uint16_t)std::stoi(port_part);
         server_host_address = host_part;
     }
@@ -240,10 +178,28 @@ int main(int argc, char *argv[])
    
     try
     {
-        boost::asio::io_context io_context;
-        udp_server server1(io_context, serverPort1);
-        tcp_server server2(io_context, serverPort2);
-        io_context.run();
+        //boost::asio::io_context io_context_udp;
+        boost::asio::io_context io_context_tcp;
+
+        std::thread debug_2(debug_1);
+        debug_2.detach();
+        std::thread udp_thread([port_listening_gui, port_sending_gui]()
+            {
+                boost::asio::io_context udp_io_context;
+                udp_server(udp_io_context, port_listening_gui, port_sending_gui);
+            });
+        udp_thread.detach();
+
+        //std::thread udp_thread(udp_server, io_context_udp, port_listening_gui, port_sending_gui);
+
+        std::thread tcp_thread([port_server]() 
+            {
+                boost::asio::io_context io_context_tcp;
+                tcp_server(io_context_tcp, port_server); 
+            });
+        tcp_thread.detach();
+
+        //std::thread tcp_thread(tcp_server, io_context_tcp, port_server);
     }
     catch (std::exception& e)
     {
